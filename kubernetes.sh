17d4e1940774254762ff4857191d8923155b611d
Pushkar Sharma <pushkarsharma1994@gmail.com>
2020-04-03 01:31:22 -0700

    Kubernetes installation partial automation

---
+++ kubernetes.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sudo su
+swapoff -a
+vim /etc/fstab
+vim /etc/hosts
+
+apt-get update && sudo apt-get install -y apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+
+apt-get update
+apt-get install -y kubelet kubeadm kubectl
+apt-mark hold kubelet kubeadm kubectl
+
+# Add Environment="cgroup-driver=systemd/cgroup-driver=cgroupfs" in the file
+vim /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
