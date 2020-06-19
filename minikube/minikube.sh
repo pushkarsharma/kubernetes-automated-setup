d6b13381af92255ea1bd15dc26cba45929d81e31
pushkarsharma <pushkarsharma1994@gmail.com>
2020-06-19 15:19:26 -0700

    Update minikube.sh

--- minikube/minikube.sh
+++ minikube/minikube.sh
@@ -4,7 +4,6 @@ curl -LO https://storage.googleapis.com/minikube/releases/latest/docker-machine-
 sudo install docker-machine-driver-kvm2 /usr/local/bin/
 rm docker-machine-driver-kvm2
 
-
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
 rm minikube-linux-amd64
