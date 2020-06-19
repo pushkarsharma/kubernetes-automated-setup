40ee2f115865d587db22cd439694af182c479409
pushkarsharma <pushkarsharma1994@gmail.com>
2020-06-19 03:29:10 -0700

    Updated comment for more readability

--- kubernetes.sh
+++ kubernetes.sh
@@ -15,5 +15,5 @@ apt-get update
 apt-get install -y kubelet kubeadm kubectl
 apt-mark hold kubelet kubeadm kubectl
 
-# Add Environment="cgroup-driver=systemd/cgroup-driver=cgroupfs" in the file
+# Add    Environment="cgroup-driver=systemd/cgroup-driver=cgroupfs"    in the file
 vim /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
